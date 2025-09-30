4c7125a3738abfe1773dfd478850f6e61d32a0b5
Mauricio Donoso <donosomauricio721@gmail.com>
2025-09-30 01:02:25 -0300

    Cap.-154-Listo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localeES from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeES,'es');
+registerLocaleData(localeES,'fr');
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+     provideRouter(routes),
+
+     {
+      provide: LOCALE_ID,
+      useValue: 'fr',  
+     },
+
+    ],
+
 };
