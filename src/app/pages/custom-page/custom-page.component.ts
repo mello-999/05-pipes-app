bd5b8c8339c5dd2734b5c64f711d3b7ddf5bc045
Mauricio Donoso <donosomauricio721@gmail.com>
2026-02-10 23:07:57 -0300

    Cap.173-Listo

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -6,6 +6,8 @@ import { HeroColorPipe } from '../../pipes/hero-color.pipe';
 import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
 import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interfaces';
 
 
 @Component({
@@ -17,7 +19,8 @@ import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
     HeroColorPipe,
     HeroTextColorPipe,
     TitleCasePipe,
-    HeroCreatorPipe
+    HeroCreatorPipe,
+    HeroSortByPipe
 
   ],
   templateUrl: './custom-page.component.html',
@@ -32,6 +35,8 @@ toggleUpperCase() {
 }
 
 heroes = signal(heroes)
+
+sortBy = signal<keyof Hero | null>(null)
 
 }
 
